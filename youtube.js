285d1eee6c4696320f046d5024540bbf656dc2ae
qmmakau <47814455+qmmakau@users.noreply.github.com>
2019-04-16 15:27:10 +0100

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,46 +1,24 @@
-// load the IFrame Player API code asynchronously
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var ytPlayers = new Array();
-var ytPlayerStates = new Array();
-
-window.onYouTubePlayerAPIReady = function()
-{
-    jQuery('.video-container>iframe').each(function(index)
-    {
-        var iframeID = jQuery(this).attr('id');
-        ytPlayerStates[iframeID] = -1;
-
-        (function (iframeID) {
-            ytPlayers.push(new YT.Player(iframeID, { 
-                events:
+var playerCurrentlyPlaying = null;
+function chain(){       
+    $('.post').each(function(){
+        var player_id = $(this).children('iframe').attr("id");
+        player = new YT.Player( player_id, { 
+            events:
                 {      
-                    'onStateChange' : function (event) 
+                'onStateChange': function (event) 
                     {
-                        console.log(iframeID+' '+event.data);
 
-                        if (event.data == 1)
-                        {
-                            ytPlayerStates[iframeID] = 1;
-                            for (var key in ytPlayerStates)
-                            {
-                                if (key != iframeID)
-                                {
-                                    if (ytPlayerStates[key] == 1 || ytPlayerStates[key] == 3)
-                                    {
-                                        ytPlayerStates[key] = 2;
-                                        callPlayer(key, 'pauseVideo')
-                                    }
-                                }
-                                console.log(key+' : '+ytPlayerStates[key]);
-                            };
-                        }
+                    if (event.data == YT.PlayerState.PLAYING) 
+                        { 
+                          if(playerCurrentlyPlaying != null && 
+                          playerCurrentlyPlaying != player_id)
+                          callPlayer( playerCurrentlyPlaying , 'pauseVideo' );
+                          playerCurrentlyPlaying = player_id;
+
+                        }            
+
                     }
-                }
-            }));
-        })(iframeID); 
+                }        
+        });
     });
 }
