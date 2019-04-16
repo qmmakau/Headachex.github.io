7938ded5b2bc5c3571f96bca4949cb71fb1c6891
qmmakau <47814455+qmmakau@users.noreply.github.com>
2019-04-16 16:01:17 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -36,34 +36,3 @@
                 }
             }
 
-
-players = new Array();
-
-function onYouTubeIframeAPIReady() {
-    var temp = $("iframe.yt_players");
-    for (var i = 0; i < temp.length; i++) {
-        var t = new YT.Player($(temp[i]).attr('id'), {
-            events: {
-                'onStateChange': onPlayerStateChange
-            }
-        });
-        players.push(t);
-    }
-
-}
-onYouTubeIframeAPIReady();
-
-
-function onPlayerStateChange(event) {
-
-    if (event.data == YT.PlayerState.PLAYING) {
-        //alert(event.target.getVideoUrl());
-       // alert(players[0].getVideoUrl());
-        var temp = event.target.getVideoUrl();
-        var tempPlayers = $("iframe.yt_players");
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
-
-        }
-    }
-}
